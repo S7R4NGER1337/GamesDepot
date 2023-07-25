86619c921fcf1815334dfb0a636182f34cab377a
S7R4NGER3301 <nikolakoew@gmail.com>
2023-07-25 17:19:43 +0300

    adding create game logic

--- GamesDepot/src/app/home/trending/trending.component.ts
+++ GamesDepot/src/app/home/trending/trending.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { addDoc, collection } from 'firebase/firestore';
 
 @Component({
   selector: 'app-trending',
   templateUrl: './trending.component.html',
   styleUrls: ['./trending.component.css']
 })
-export class TrendingComponent {
 
+export class TrendingComponent {
+  
+  constructor(private fs: Firestore){}
+
+  addGame(game: any){
+    const collectionInstance = collection(this.fs, 'games')
+    addDoc(collectionInstance, game).then(() => {
+      console.log('Data saved')
+    }).catch((err)=>console.log(err))
+  }
 }
