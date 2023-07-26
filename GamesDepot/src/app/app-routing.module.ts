9853b0f24f69e48c0dcc3fb6ebf0e54b1e495fc5
S7R4NGER3301 <nikolakoew@gmail.com>
2023-07-27 00:18:22 +0300

    updating create html and css and downloading new webfonts

--- GamesDepot/src/app/app-routing.module.ts
+++ GamesDepot/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
+import { CreateGameComponent } from './create-game/create-game.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: AppComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'create', component: CreateGameComponent}
 ];
 
 @NgModule({
