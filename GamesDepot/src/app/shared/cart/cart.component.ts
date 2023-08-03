7299f0e2c5dd9efbbb4659a6ed8663a0b853496c
S7R4NGER3301 <nikolakoew@gmail.com>
2023-08-03 16:09:04 +0300

    making car rendering data dynamically

--- GamesDepot/src/app/shared/cart/cart.component.ts
+++ GamesDepot/src/app/shared/cart/cart.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
+export class CartComponent implements OnInit{
 
+  games: any = []
+  cartInfo: any = {}
+
+  constructor(private apiService: ApiService){}
+
+  ngOnInit(): void {
+    this.renderCartItems()
+  }
+
+  renderCartItems(){
+    const games = Object.values(sessionStorage)
+    let price: any = 0
+    let gamesCounter: any = 0
+    games.forEach(el => {
+      if(el == undefined){
+        return
+      }
+      this.apiService.getGameById(el).then(res => {
+        const game: any = res[0]
+        this.games.push(game) 
+        
+        price += game['price']
+        gamesCounter ++
+        this.cartInfo = {price, gamesCounter}
+     })
+    })
+  }
 }
